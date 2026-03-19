512c7b538f73bc7d3b45d538befe761e9c7582cc
ajumoko1984 <bamideleolawale13@gmail.com>
2026-03-19 19:19:52 +0100

    Deployment issue

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'https://unischedule.vercel.app',
+  origin: process.env.CLIENT_URL || 'https://unischedule-two.vercel.app',
   credentials: true,
 }));
 app.use(express.json());
